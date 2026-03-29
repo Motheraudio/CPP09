3eb491aec6bce48f025c0cc9f1683aecc086659e
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-03-29 17:41:46 +0200

    added early return

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,7 +1,9 @@
 #include "RPN.hpp"
 int main(int argc, char **argv) {
-  if (argc != 2)
+  if (argc != 2) {
     std::cerr << "use: ./RPN \"Expression\"" << std::endl;
+    return (1);
+  }
   try {
     std::cout << RPN::FetchResult(argv[1]) << std::endl;
   } catch (std::exception &e) {
