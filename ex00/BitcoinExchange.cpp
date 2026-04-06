69f4aecf6855f0523f946f2d9e22e716765ce5e1
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-04-06 20:37:45 +0200

    testing block rule

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -13,7 +13,7 @@ BitcoinExchange::BitcoinExchange(char *resolve)
     this->_Cdb = createMap();
     this->_Cresolve = createMap(resolve);
   }
-  catch (std::exception)
+  catch (std::exception &e)
   {
     throw;
   }
